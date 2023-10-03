37dc84f72b707bd5edb75ea8f9c58536fc034f7a
SanBran <luminoforos@gmail.com>
2023-10-02 18:18:47 -0600

    Deploy DB

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+server.use(express.urlencoded({ extended: true }));
 
 server.use("/", routes);
 

5819858bda05125f9d1757d3e1861de95aeeb160
Brandon Galarza <121001322+SanBran@users.noreply.github.com>
2025-02-17 23:24:28 -0600

    Comporbación en base de datos

--- src/controllers/getInfoApi.js
+++ src/controllers/getInfoApi.js
@@ -2,21 +2,27 @@ const axios = require("axios");
 const { Country } = require("../db");
 
 const getInfoApi = async () => {
-  const countriesApi = (await axios(`https://restcountries.com/v3/all`)).data;
-  const cleanCountries = await countriesApi.map((e) => {
-    let capital = e.capital ? e.capital[0] : null;
-    let newCountry = Country.create({
-      id: e.cca3,
-      name: e.name.common,
-      flag: e.flags[1],
-      continent: e.continents[0],
-      capital: capital,
-      subregion: e.subregion,
-      area: e.area,
-      population: e.population,
+
+  if (!Country.findOne()) {
+    const countriesApi = (await axios(`https://restcountries.com/v3/all`)).data;
+    const cleanCountries = await countriesApi.map((e) => {
+      let capital = e.capital ? e.capital[0] : null;
+      let newCountry = Country.create({
+        id: e.cca3,
+        name: e.name.common,
+        flag: e.flags[1],
+        continent: e.continents[0],
+        capital: capital,
+        subregion: e.subregion,
+        area: e.area,
+        population: e.population,
+      });
     });
-  });
-  console.log("Database filled")
+    console.log("Database filled")
+  } else {
+    console.log("Database already filled")
+    
+  }
 };
 
 module.exports = getInfoApi;
